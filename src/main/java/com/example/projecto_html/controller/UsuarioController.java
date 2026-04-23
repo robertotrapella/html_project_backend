aef76be327543c2feaeecb50745ea23658862aec
robertotrapella <roberto.trapella.gut@gmail.com>
2026-04-22 21:43:53 -0600

    Cosas

--- src/main/java/com/example/projecto_html/controller/UsuarioController.java
+++ src/main/java/com/example/projecto_html/controller/UsuarioController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 import java.util.Map;
 import java.util.HashMap;
+import org.mindrot.jbcrypt.BCrypt;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -17,6 +20,16 @@ public class UsuarioController {
 
     @PostMapping("/registro")
     public String registrarUsuario(@RequestBody usuario nuevoUsuario) {
+        Optional<usuario> usuarioExistente = usuarioRepository.findByUsername(nuevoUsuario.getUsername());
+        if (!usuarioExistente.isEmpty()) {
+            return "Error: Usuario ya ocupado";
+        }
+        Optional<usuario> emailExistente = usuarioRepository.findByEmail(nuevoUsuario.getEmail());
+        if (!emailExistente.isEmpty()) {
+            return "Error: Correo ya registrado";
+        }
+        String contrasenaEncriptada = BCrypt.hashpw(nuevoUsuario.getPassword(), BCrypt.gensalt());
+        nuevoUsuario.setPassword(contrasenaEncriptada);
         usuarioRepository.save(nuevoUsuario);
         return "¡Usuario guardado con éxito!";
     }
@@ -30,9 +43,13 @@ public class UsuarioController {
             return respuesta;
         }
         usuario usuarioReal = usuarioEncontrado.get();
-        if (usuarioReal.getPassword().equals(credenciales.getPassword()) && !usuarioEncontrado.isEmpty()) {
+        if (BCrypt.checkpw(credenciales.getPassword(), usuarioReal.getPassword()) && !usuarioEncontrado.isEmpty()) {
+            Algorithm algorithm = Algorithm.HMAC256("miSecretoAbsoluto");
+            String token = JWT.create().withIssuer("projecto_html_auth")
+                    .withClaim("username", usuarioReal.getUsername()).sign(algorithm);
             respuesta.put("mensaje", "Exito: Usuario logueado correctamente");
             respuesta.put("email", usuarioReal.getEmail());
+            respuesta.put("token", token);
             return respuesta;
         } else {
             respuesta.put("mensaje", "Error: Contraseña o usuario incorrectos");
--- src/main/java/com/example/projecto_html/repository/UsuarioRepository.java
+++ src/main/java/com/example/projecto_html/repository/UsuarioRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<usuario, Long> {
     Optional<usuario> findByUsername(String username);
+
+    Optional<usuario> findByEmail(String email);
 }
